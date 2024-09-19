2add6e3e38ed0b7013008da7767bc14d259687ea
krishan190 <krishnanamdeo2001@gmail.com>
2024-09-19 16:03:20 +0530

    season 3 Laying the foundation

--- App.js
+++ App.js
@@ -1,41 +1,22 @@
-{
-  /* <div id="container">
-      <h1 id="title">Heading 1</h1>
-      <h1 id="title">Heading 2</h1>
-  </div> */
-}
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1"
+const Title = (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2] //It is a react element (It is a object at the end of the day)
-);
-
-console.log(heading);
+const HeadingCmp = () => {
+  return (
+    <div>
+      {Title}
+      <h1>This is a functional component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// passing a react element inside the root
-// render will modify the DOM
-root.render(container);
+root.render(<HeadingCmp />);
